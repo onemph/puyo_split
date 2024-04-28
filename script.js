ff6baa8761557c7ab2a22f10e2465e859fd9d771
onemph <144752020+onemph@users.noreply.github.com>
2024-04-29 01:39:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 function splitText() {
     var inputText = document.getElementById('inputText').value;
     var outputDiv = document.getElementById('output');
-
-    var copyButton = document.createElement('button');
-    copyButton.textContent = 'コピー済';
-    outputDiv.appendChild(copyButton);
-    var copyButtonWidth = copyButton.getBoundingClientRect().width;
-
+    
     outputDiv.innerHTML = '';
-
+    
     var tmpText = '';
     var outputText = '';
 
@@ -24,7 +19,7 @@ function splitText() {
 
             if (totalLength >= 128) {
                 splitCount++;
-                addTextToOutput(outputDiv, outputText, splitCount, copyButtonWidth);
+                addTextToOutput(outputDiv, outputText, splitCount);
                 outputText = '';
             }
 
@@ -35,18 +30,18 @@ function splitText() {
 
     if (outputText.length > 0) {
         splitCount++;
-        addTextToOutput(outputDiv, outputText, splitCount, copyButtonWidth);
+        addTextToOutput(outputDiv, outputText, splitCount);
     }
 }
 
-function addTextToOutput(outputDiv, text, splitCount, copyButtonWidth) {
+function addTextToOutput(outputDiv, text, splitCount) {
     var formattedText = text.replace(/\n/g, "<br>");
-
+    
     var div = document.createElement('div');
     div.innerHTML = `
         <div data-index="${splitCount - 1}" style="border-top: 1px solid #ccc; padding-top: 10px;">
             <div style="display: flex; align-items: center;">
-                <button onclick="copyText(${splitCount - 1})" style="width: ${copyButtonWidth}px;">コピー</button>
+                <button id="copyButton-${splitCount - 1}" onclick="copyText(${splitCount - 1})">コピー</button>
                 <p>${splitCount}</p>
             </div>
             <div>
@@ -55,18 +50,20 @@ function addTextToOutput(outputDiv, text, splitCount, copyButtonWidth) {
             </div>
         </div>
     `;
-
+    
     outputDiv.appendChild(div);
 }
 
 function copyText(index) {
-    var textElement = document.querySelectorAll(`#output div[data-index="${index}"]`);
-    var specificDiv = textElement[0];
-    alert(specificDiv.outerHTML + '\n\n' + specificDiv.querySelector('div:nth-of-type(1) p:first-of-type').outerHTML + '\n\n' + specificDiv.querySelector('div:nth-of-type(2) p:first-of-type').outerHTML);
-    var text = specificDiv.querySelector('div:nth-of-type(2) p:first-of-type').innerText;
+    var textElement = document.querySelector(`#output div[data-index="${index}"] p:first-of-type`);
+    var text = textElement.innerText;
     navigator.clipboard.writeText(text)
         .then(() => {
-            var button = specificDiv.querySelector('button');
+            var nextButton = document.getElementById(`copyButton-${index + 1}`);
+            if (nextButton) {
+                nextButton.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            }
+            var button = textElement.parentElement.parentElement.querySelector('button');
             button.textContent = 'コピー済';
         })
         .catch(err => {
