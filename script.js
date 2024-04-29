1bb5b50040a78236defb91a557613078b86a8fa6
onemph <144752020+onemph@users.noreply.github.com>
2024-04-29 15:02:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function splitText() {
     
     var tmpText = '';
     var outputText = '';
-
     var splitCount = 0;
 
     for (var i = 0; i < inputText.length; i++) {
@@ -15,13 +14,14 @@ function splitText() {
         tmpText += currentChar;
 
         if (currentChar === '。' || currentChar === '\n' || i === inputText.length - 1) {
-            outputText = outputText.replace(/^\n*/, '');
             var totalLength = tmpText.length + outputText.length;
+            var lineCount = (outputText.match(/\n/g) || []).length;
 
-            if (totalLength >= 128) {
+            if (totalLength >= 128 || lineCount >= 10) {
                 splitCount++;
                 addTextToOutput(outputDiv, outputText, splitCount);
                 outputText = '';
+                tmpText = '';
             }
 
             outputText += tmpText;
@@ -31,6 +31,7 @@ function splitText() {
 
     if (outputText.length > 0) {
         splitCount++;
+        outputText = outputText.replace(/^\n*/, '');
         addTextToOutput(outputDiv, outputText, splitCount);
     }
 }
